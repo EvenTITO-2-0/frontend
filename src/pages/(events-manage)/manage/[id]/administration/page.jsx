166fbcc2703b612212f3a001b693064119b964d7
Val <vcorrea@fi.uba.ar>
2025-09-24 01:05:28 -0300

    force refresh

--- src/pages/(events-manage)/manage/[id]/administration/page.jsx
+++ src/pages/(events-manage)/manage/[id]/administration/page.jsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { useQueryClient } from '@tanstack/react-query'
 import ContainerPage from '@/pages/(events-manage)/_components/containerPage'
 import DatesCheck from './_components/DatesCheck'
 import EventDetails from './_components/EventDetails'
@@ -6,6 +9,22 @@ import { getDates, getEventStatus, getWorksStatus } from './_components/utils'
 import StepsForPublish from './_components/StepsForPublish'
 
 export default function Page({ eventInfo, inscriptions }) {
+  const [searchParams] = useSearchParams()
+  const queryClient = useQueryClient()
+
+  useEffect(() => {
+    const linked = searchParams.get('linked')
+    if (linked === '1') {
+      queryClient.invalidateQueries({
+        // fuerza refresh de los datos
+        queryKey: ['getEventById', { eventId: eventInfo.id }],
+      })
+      const newUrl = new URL(window.location)
+      newUrl.searchParams.delete('linked')
+      window.history.replaceState({}, '', newUrl)
+    }
+  }, [searchParams, queryClient, eventInfo.id])
+
   return (
     <ContainerPage>
       <div className="space-y-6">
